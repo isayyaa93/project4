d1399bc586ec64439d923aab923e4d6215a205a6
Isaiah Kang <i.say.yaa@gmail.com>
2022-06-03 10:09:43 -0400

    styled some more2

--- client/src/pages/NewBlog.js
+++ client/src/pages/NewBlog.js
@@ -7,7 +7,7 @@ import { Button, Error, FormField, Input, Label, Textarea } from "../styles";
 function NewBlog({ user }) {
   const [title, setTitle] = useState("My Awesome Blog");
   const [minutesToComplete, setMinutesToComplete] = useState("30");
-  const [instructions, setInstructions] = useState(``);
+  const [instructions, setInstructions] = useState(`  `);
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const history = useHistory();
